de813da0f3da4c7af2d937f7b6e42dc2ee307dad
Enthrall Records <info@enthrallrecords.com>
2019-09-04 20:02:48 -0400

    adapt to newsletter changes

--- components/core/blocks/Footer/Newsletter.vue
+++ components/core/blocks/Footer/Newsletter.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="newsletter py25 px15 bg-cl-secondary">
+  <div class="newsletter py25 px15 bg-cl-secondary" v-if="!isSubscribed">
     <div class="container">
       <div class="newsletter-content m0 row middle-sm start-md">
         <div class="col-md-9 col-xs-12">
@@ -18,17 +18,19 @@
         </div>
       </div>
     </div>
-    <newsletter-popup v-if="loadNewsletterPopup"/>
+    <newsletter-popup v-if="loadNewsletterPopup" />
   </div>
 </template>
 
 <script>
+import SubscriptionStatus from '@vue-storefront/core/modules/newsletter/mixins/SubscriptionStatus'
 import ButtonOutline from 'theme/components/theme/ButtonOutline'
 import { mapState } from 'vuex'
 const NewsletterPopup = () => import(/* webpackChunkName: "vsf-newsletter-modal" */ 'theme/components/core/NewsletterPopup.vue')
 
 export default {
   name: 'Newsletter',
+  mixins: [SubscriptionStatus],
   data () {
     return {
       loadNewsletterPopup: false
@@ -36,14 +38,10 @@ export default {
   },
   computed: {
     ...mapState({
-      isOpen: state => state.ui.newsletterPopup,
-      isSubscribed: state => state.mailchimp.isSubscribed
+      isOpen: state => state.ui.newsletterPopup
     })
   },
   methods: {
-    newsletterClick () {
-      this.$store.commit('ui/setNewsletterPopup', !this.isOpen)
-    },
     showNewsletterPopup () {
       this.loadNewsletterPopup = true
       this.$bus.$emit('modal-show', 'modal-newsletter')
@@ -57,16 +55,18 @@ export default {
 </script>
 
 <style scoped>
-  @media (max-width: 1023px) {
-    .newsletter-button {
-      padding-top: 25px;
-      text-align: center;
+  @media (min-width: 767px) and (max-width: 1200px){
+    .button-outline{
+      min-width: 100%;
     }
   }
-
   @media (max-width: 767px) {
     .h3 {
       font-size: 18px;
+      text-align: center;
+    }
+    .newsletter-button {
+      padding-top: 25px;
       text-align: center;
     }
   }
